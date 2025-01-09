34759262ec4475e37eeed60ec60cc48f2532cf6a
Avery-Hat <biskatarius@gmail.com>
2025-01-09 15:00:37 -0800

    added texttohtml and provided tests. updated textnode for proper image formatting

--- src/textnode.py
+++ src/textnode.py
@@ -9,14 +9,24 @@ class TextType(Enum):
     LINK = "link"
     IMAGE = "image"
 
-
 class TextNode:
     def __init__(self, text, text_type, url=None):
-        self.text = text
+        if text_type == TextType.IMAGE:
+            self.source_path = text
+            self.alt_text = url
+        else:
+            self.text = text
+            self.url = url
+        
         self.text_type = text_type
-        self.url = url
 
     def __eq__(self, other):
+        if self.text_type == TextType.IMAGE:
+            return (
+                self.text_type == other.text_type
+                and self.source_path == other.source_path
+                and self.alt_text == other.alt_text
+            )
         return (
             self.text_type == other.text_type
             and self.text == other.text
@@ -24,4 +34,23 @@ class TextNode:
         )
 
     def __repr__(self):
+        if self.text_type == TextType.IMAGE:
+            return f"TextNode(source_path={self.source_path}, {self.text_type.value}, alt_text={self.alt_text})"
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+    #PREVIOUS CODE WHICH HAD SRC AND TXT SWAPPED BY ACCIDENT.
+# class TextNode:
+#     def __init__(self, text, text_type, url=None):
+#         self.text = text
+#         self.text_type = text_type
+#         self.url = url
+
+#     def __eq__(self, other):
+#         return (
+#             self.text_type == other.text_type
+#             and self.text == other.text
+#             and self.url == other.url
+#         )
+
+#     def __repr__(self):
+#         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
