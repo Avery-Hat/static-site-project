f8b45dbb0e983b637db6d41e1a95a1a0b16f54e9
Avery-Hat <biskatarius@gmail.com>
2025-02-13 18:01:31 -0800

    chapter 5, part 1. wrote copying code in main, tested with static folder pulled from assignment.

--- src/main.py
+++ src/main.py
@@ -24,15 +24,6 @@ def copy_static_directory(src, dst):
 
 def main():
     copy_static_directory("static", "public")
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
-
-    # Let's add some test cases
-    test_images = "Here's ![cute bear](https://bears.com/cute.jpg) and ![cool cat](https://cats.com/cool.jpg)"
-    test_links = "Here's [my website](https://mysite.com) and [Boot.dev](https://boot.dev)"
-    
-    print(extract_markdown_images(test_images))
-    print(extract_markdown_links(test_links))
 
 if __name__ == "__main__":
     main()
