3821a71e2b3699508133983a251ca58d0ccf2560
Avery-Hat <biskatarius@gmail.com>
2025-02-13 17:46:04 -0800

    Remove public directory from Git tracking

--- src/main.py
+++ src/main.py
@@ -1,6 +1,29 @@
 from textnode import TextNode, TextType
 from markdown_parser import *
+import os
+import shutil
+
+def copy_static_directory(src, dst):
+    # Cleaning out destination if it exists
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    # Making the fresh destination directory
+    os.mkdir(dst)
+
+    # Get list of all items in source directory
+    for item in os.listdir(src):
+        # Create the full paths for source and destination
+        src_path = os.path.join(src, item)
+        dst_path = os.path.join(dst, item)
+
+        if os.path.isfile(src_path):
+            shutil.copy(src_path,dst_path)
+        else:
+            copy_static_directory(src_path, dst_path)
+
+
 def main():
+    copy_static_directory("static", "public")
     node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(node)
 
